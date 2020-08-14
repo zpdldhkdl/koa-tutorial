3bd4b287f1823544a187813d88e64988ee7b5d07
DESKTOP-A43AVN4\kyong <lklklnm@daum.net>
2020-08-15 01:54:51 +0900

    app.js

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const KoaRouter = require('koa-router');
 const json = require('koa-json');
 const path = require('path')
 const render = require('koa-ejs');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new KoaRouter();
 
 // Replace with DB
 const things = ['My Family', 'Programming', 'Music']
+
 // Json Prettier Middleware
 app.use(json());
+// BodyParser Middleware
+app.use(bodyParser());
+
+app.context.user = 'Test';
 
 // Simple Middleware Example
 // app.use(async ctx => ctx.body = { msg: 'Hello World' });
@@ -26,7 +32,7 @@ render(app, {
 // Routes
 router.get('/', index);
 router.get('/add', showAdd);
-router.post('/add', add)
+router.post('/add', add);
 
 // List of things
 async function index(ctx) {
@@ -41,7 +47,17 @@ async function showAdd(ctx) {
     await ctx.render('add');
 }
 
-router.get('/test', ctx => ctx.body = 'Hello Test');
+// post data from add
+async function add(ctx) {
+    const body = await ctx.request.body;
+
+    things.push(body.thing);
+
+    ctx.redirect('/');
+}
+
+router.get('/test', ctx => ctx.body = `Hello ${ctx.user}`);
+router.get('/test2/:id', ctx => ctx.body = `Hello ${ctx.params.id}`);
 
 //Router Middleware
 app.use(router.routes()).use(router.allowedMethods());
